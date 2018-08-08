49e2381d8d96ceddfe71d9f479fd4d592e60e5e9
ana caroline <acarolinepassos@icloud.com>
2018-08-08 13:49:05 -0300

    z
    z

---
+++ estoque/Database/MongoConnection.cs
@@ -0,0 +1,50 @@
+﻿using Database.Properties;
+using Model;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace Database
+{
+    public class MongoConnection
+    {
+        protected static IMongoDatabase GetConnection()
+        {
+            return new MongoClient(Settings.Default.connectionString).GetDatabase(Settings.Default.name);
+        }
+
+
+        public static bool InsertOne<BsonDocument>(string name, BsonDocument document)
+        {
+            try
+            {
+                IMongoDatabase _database = GetConnection();
+
+                var collection = _database.GetCollection<BsonDocument>(name);
+                collection.InsertOne(document);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static List<BsonDocument> QueryCollection<BsonDocument>(string name, FilterDefinition<BsonDocument> filter, int? limit)
+        {
+            try
+            {
+                IMongoDatabase _database = GetConnection();
+
+                return _database.GetCollection<BsonDocument>(name).Find(filter).Limit(limit).ToListAsync().Result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+}
--- estoque/Model/Estoque.cs
+++ estoque/Model/Estoque.cs
@@ -8,5 +8,21 @@ namespace Model
 {
     public class Estoque
     {
+        public ObjectId _id { get; set; }
+        public string Codigo { get; set; }
+        public string ClasFiscal { get; set; }
+        public string setor { get; set; }
+        public string PaymentMethod { get; set; }
+        public string codAlf { get; set; }
+        public string versao { get; set; }
+        public string Email { get; set; }
+        public string partNumb { get; set; }
+        public string produto { get; set; }
+        public string descricao { get; set; }
+        public string fabricante { get; set; }
+        public string NF1 { get; set; }
+        public string fornecedor { get; set; }
+        public DateTime dataCompra { get; set; }
+        public double valorNF { get; set; }
     }
 }
