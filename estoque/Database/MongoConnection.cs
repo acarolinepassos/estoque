9c9933a582532990f515bc7b3aeaca015e8e84a5
ana caroline <acarolinepassos@icloud.com>
2018-08-08 14:33:40 -0300

    s

--- estoque/Database/MongoConnection.cs
+++ estoque/Database/MongoConnection.cs
@@ -5,6 +5,7 @@ using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 
+
 namespace Database
 {
     public class MongoConnection
@@ -38,6 +39,40 @@ namespace Database
                 IMongoDatabase _database = GetConnection();
 
                 return _database.GetCollection<BsonDocument>(name).Find(filter).Limit(limit).ToListAsync().Result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static bool DeleteOne<BsonDocument>(string name, FilterDefinition<BsonDocument> filter)
+        {
+            try
+            {
+                IMongoDatabase _database = GetConnection();
+
+                var collection = _database.GetCollection<BsonDocument>(name);
+                collection.DeleteOne(filter);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static bool ReplaceOne<BsonDocument>(string name, FilterDefinition<BsonDocument> filter, BsonDocument document)
+        {
+            try
+            {
+                IMongoDatabase _database = GetConnection();
+
+                var collection = _database.GetCollection<BsonDocument>(name);
+                collection.ReplaceOne(filter, document);
+
+                return true;
             }
             catch (Exception ex)
             {
