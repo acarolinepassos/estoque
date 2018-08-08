7dcec9366107fadfdebfbcc6313ec5213fdb68c7
ana caroline <acarolinepassos@icloud.com>
2018-08-08 10:25:18 -0300

    es
    ss

--- estoque/estoque/Estoque.cs
+++ estoque/estoque/Estoque.cs
@@ -8,5 +8,20 @@ namespace Model
 {
     public class Estoque
     {
+        public ObjectId _id { get; set; }
+        public string Codigo { get; set; }
+        public string ClasFiscal { get; set; }
+        public string Setor { get; set; }
+        public string CodAlf { get; set; }
+        public string Versao { get; set; }
+        public string PartNumber { get; set; }
+        public string Produto { get; set; }
+        public string Descricao { get; set; }
+        public string Fabricante { get; set; }
+        public string NF1 { get; set; }
+        public string Fornecedor { get; set; }
+        public DateTime dataCadastro { get; set; }
+        public double valorNF { get; set; }
+
     }
 }
